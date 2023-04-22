e0acb21129a7054f91773fe0efae99b33be94975
Entropy98 <harperweigle@gmail.com>
2023-04-22 13:47:29 -0700

    it doesn't seem that seperate build folders helps with the sides

--- firmware/scripts/build.sh
+++ firmware/scripts/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-mkdir -p build/right
-cd build/right
-cmake -DPICO_BOARD=pico -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DKBDSIDE=right ../..
+mkdir -p build
+cd build
+
+cmake -DPICO_BOARD=pico -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DKBDSIDE=right ..
 cmake --build .
 
-cd ..
-mkdir -p left
-cd left
-
-cmake -DPICO_BOARD=pico -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DKBDSIDE=left ../..
+cmake -DPICO_BOARD=pico -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DKBDSIDE=left ..
 cmake --build .
